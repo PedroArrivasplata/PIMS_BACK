300bacc4cae39d7ffc9b0d897795529ccd0cbe76
CESARleo18 <espinozadiazcesaragusto@gmail.com>
2025-06-20 12:00:18 -0500

    pantallas subidas

---
+++ Veterinaria/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="vet-dashboard">
+    <Sidebar />
+    <div class="vet-main-content scroll-sections">
+      <router-view /> </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+  },
+};
+</script>
+
