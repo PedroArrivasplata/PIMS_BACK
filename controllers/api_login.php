eaf4e8bb613f4e1e96b8c5e65855427c3ceb3045
PedroArrivasplata <pedro.arrivasplata.edu@hotmail.com>
2025-06-21 21:55:58 -0500

    Update api_login.php

--- controllers/api_login.php
+++ controllers/api_login.php
@@ -1,4 +1,11 @@
 <?php
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 require '../models/funcionesUsuario.php';
 
 header("Content-Type: application/json; charset=UTF-8");
